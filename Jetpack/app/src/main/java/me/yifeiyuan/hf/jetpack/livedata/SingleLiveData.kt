bc5f92e632efc23d4cc2a950bceaafbdbf4cd8e9
mingjue <zhening.czn@alibaba-inc.com>
2021-09-29 10:59:58 +0800

    u

---
+++ Jetpack/app/src/main/java/me/yifeiyuan/hf/jetpack/livedata/SingleLiveData.kt
@@ -0,0 +1,10 @@
+package me.yifeiyuan.hf.jetpack.livedata
+
+import androidx.lifecycle.MutableLiveData
+
+/**
+ * Created by 程序亦非猿 on 2021/9/29.
+ */
+class SingleLiveData<T> : MutableLiveData<T>() {
+
+}
---
+++ Jetpack/app/src/main/java/me/yifeiyuan/hf/jetpack/viewmodel/AppViewModel.kt
@@ -0,0 +1,13 @@
+package me.yifeiyuan.hf.jetpack.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+/**
+ * Created by 程序亦非猿 on 2021/9/29.
+ */
+class AppViewModel(application: Application) :AndroidViewModel(application){
+
+
+
+}
